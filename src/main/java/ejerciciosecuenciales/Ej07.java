e820e838800dbe269b71acf242dc7b17101a6e8a
cristinaj1 <cristinacee305@gmail.com>
2021-10-13 17:32:04 +0200

    cambiado el nombre del pack

--- src/main/java/ejerciciosecuenciales/Ej07.java
+++ src/main/java/ejerciciosecuenciales/Ej07.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ejerciciosSecuenciales;
+package ejerciciosecuenciales;
 
 import java.util.Scanner;
 
@@ -10,22 +10,23 @@ import java.util.Scanner;
  *
  * @author cristina
  */
-public class Ej08 {
+public class Ej07 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        final double IVA = 0.16;
+
+        final double GALON = 3.78;
+        final double PRECIO = 1.20;
 
         //Pedimos datos por pantalla
-        System.out.println("Introduzca el precio del artículo");
-        double precio = teclado.nextDouble();
+        System.out.println("Introduzca los litros que desea valorar");
+        double litro = teclado.nextDouble();
 
         //Calculamos
-        double resultado = (precio * IVA) + precio;
+        double resultado = (litro / GALON) * PRECIO;
 
         //Mostramos resultado por pantalla
-        System.out.println("El precio con IVA es de: " + resultado);
-
+        System.out.println("El precio es de : " + resultado);
     }
 
 }
--- src/main/java/ejerciciosecuenciales/Ej08.java
+++ src/main/java/ejerciciosecuenciales/Ej08.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ejerciciosSecuenciales;
+package ejerciciosecuenciales;
 
 import java.util.Scanner;
 
@@ -10,23 +10,22 @@ import java.util.Scanner;
  *
  * @author cristina
  */
-public class Ej07 {
+public class Ej08 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
-        final double GALON = 3.78;
-        final double PRECIO = 1.20;
+        final double IVA = 0.16;
 
         //Pedimos datos por pantalla
-        System.out.println("Introduzca los litros que desea valorar");
-        double litro = teclado.nextDouble();
+        System.out.println("Introduzca el precio del artículo");
+        double precio = teclado.nextDouble();
 
         //Calculamos
-        double resultado = (litro / GALON) * PRECIO;
+        double resultado = (precio * IVA) + precio;
 
         //Mostramos resultado por pantalla
-        System.out.println("El precio es de : " + resultado);
+        System.out.println("El precio con IVA es de: " + resultado);
+
     }
 
 }
